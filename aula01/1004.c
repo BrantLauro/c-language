5b5df22c47084506393c0c32d2bc5430494e2879
BrantLauro <brantlauro@gmail.com>
2022-09-15 09:59:12 -0300

    :rocket: Aula01 done

---
+++ aula01/1004.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+    int num1, num2, prod;
+    scanf("%i", &num1); 
+    scanf("%i", &num2);
+    prod = num1 * num2; 
+    printf("PROD = %i\n", prod); 
+    return 0;
+}
