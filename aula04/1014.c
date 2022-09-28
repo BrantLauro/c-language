b14aebacbd2c2ddb9d526e1fef8a64d16fc82304
BrantLauro <brantlauro@gmail.com>
2022-09-28 18:25:51 -0300

    :rocket: 1014 done

--- aula04/1014.c
+++ aula04/1014.c
@@ -2,3 +2,14 @@
     Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) 
     e o total de combustível gasto (em litros).
 */
+
+#include <stdio.h>
+
+int main() {
+    int km;
+    double total, gas;
+    scanf("%d %lf", &km, &gas);
+    total = km / gas;
+    printf("%.3lf km/l\n", total);
+    return 0;
+}
