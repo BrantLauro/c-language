63f09ad0e5644273251b7c87541310eccc967c42
BrantLauro <brantlauro@gmail.com>
2022-09-28 18:25:38 -0300

    :rocket: 1013 done

--- aula04/1013.c
+++ aula04/1013.c
@@ -2,3 +2,15 @@
     Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. 
     Utilize a fórmula apresentada.
 */
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a, b, c, maior;
+    scanf("%d %d %d", &a, &b, &c);
+    maior = (a + b + abs(a - b)) / 2;
+    if (c > maior) maior = c;
+    printf("%d eh o maior\n", maior);
+    return 0;
+}
